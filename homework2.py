fdc7425504846c91277fefe1faf7077f2e649189
simondvdv <simondv4@gmail.com>
2024-05-27 08:54:48 +0300

    Add files via upload
    27.05.24 Практическое задание по теме "Переменные."

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_of_homework = 12
+number_of_hours = 1.5
+course_name = 'python'
+time_for_task = number_of_hours / number_of_homework
+print('Курс: ' + course_name + ', всего задач: ' + str(number_of_homework) + ', затраченно часов: '
+      + str(number_of_hours) + ', среднее время выполнения ' + str(number_of_hours / number_of_homework))
